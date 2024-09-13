e5e11c31e6490545ed3fee219c7eb4ccb4d8d6a2
Nirox <theo.mas677@gmail.com>
2024-09-13 21:34:32 +0200

    fix smooth scrolling

---
+++ index.js
@@ -0,0 +1,11 @@
+$(window).on('load', function() {
+
+    $(this).impulse();
+    $.fn.impulse.default.tempo = 500;
+    $.fn.impulse.default.range = 400;
+    $.fn.impulse.default.leap = 3;
+    $.fn.impulse.default.sloth = 3;
+    $.fn.impulse.default.sloth = 15;
+  });
+
+  /* from http://ataredo.com/morphology/lucidscroll/ */
